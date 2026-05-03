07ef821fbd767e5eecad497a427eb99ecdf15abc
Prashant <prashantdwivedipd7@gmail.com>
2026-05-03 09:17:42 +0530

    new quesiton solved

---
+++ Python/Loops/matrix.py
@@ -0,0 +1,6 @@
+matrix = [[1, 2, 3], [4, 5, 6]]
+
+
+rows = len(matrix)
+cols = len(matrix[0])
+
--- Python/Oops/class.py
+++ Python/Oops/class.py
@@ -1,11 +1,22 @@
-class Students:
-    def __init__(self,name,marks):
-        self.name = name
-        self.marks = marks
+# class Students:
+#     def __init__(self,name,marks):
+#         self.name = name
+#         self.marks = marks
 
-s1 = Students("Prashant",99)
-print(s1.name, s1.marks)
-s2 = Students("Sahil",97)
-print(s2.name,s2.marks)
+# s1 = Students("Prashant",99)
+# print(s1.name, s1.marks)
+# s2 = Students("Sahil",97)
+# print(s2.name,s2.marks)
 
 
+
+users = {"monal" : 100000}
+name = "monal"
+print(users[name])
+
+# def withdraw(name, amount):
+#     users[name] -= amount
+#     # users[name] = users[name] - amount
+#     print(name,"has :",users[name])
+
+# withdraw("monal", 1000)
