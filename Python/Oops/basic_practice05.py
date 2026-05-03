dc658f40741091658b5e2577010efb637ff08469
Prashant <prashantdwivedipd7@gmail.com>
2026-05-03 12:31:26 +0530

    new solved

--- Python/Oops/basic_practice05.py
+++ Python/Oops/basic_practice05.py
@@ -10,3 +10,23 @@
 # deposit(amount)
 # withdraw(amount) (agar balance kam ho to message print karo)
 # check_balance()
+
+
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Your current balance is: ",self.balance)
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("Low balance")
+        else:
+            print("Successful")
+
+b1 = BankAccount("Sahil",50000)
+b1.deposit(10000)
+b1.withdraw(70000)
