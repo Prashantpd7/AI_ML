20eb83b679d261e66f50dabbcb200f8f92ccc617
Prashant <prashantdwivedipd7@gmail.com>
2026-05-03 12:22:55 +0530

    new solved

--- Python/Oops/basic_practice04.py
+++ Python/Oops/basic_practice04.py
@@ -9,3 +9,23 @@
 # Methods:
 # use_phone(hours) → battery reduce kare (1 hour = -10%)
 # charge() → battery ko 100% kar de
+
+
+class Mobile:
+    def __init__(self, brand, battery):
+        self.brand = brand
+        self.battery = battery
+
+    def use_phone(self, hours):
+        for i in range(hours):
+            self.battery -= 10
+        print("Battery remaning",self.battery)
+
+    def charge(self):
+        self.battery = 100
+        print("Battery fully charged",self.battery)
+
+m1 = Mobile("Apple", 80)
+m1.use_phone(3)
+m1.charge()
+m1.use_phone(4)
