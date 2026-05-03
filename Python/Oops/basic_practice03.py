5efb71acdfe9c336ae94ba2d802cfec59d94a0ed
Prashant <prashantdwivedipd7@gmail.com>
2026-05-03 12:08:57 +0530

    new solved

--- Python/Oops/basic_practice03.py
+++ Python/Oops/basic_practice03.py
@@ -9,3 +9,22 @@
 # Methods:
 # display() → name aur salary print kare
 # increase_salary(amount) → salary me amount add kare
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    def display(self):
+        Name = self.name
+        Salary = self.salary
+        print(Name,Salary)
+    
+    def increase_salary(self, amount):
+        self.salary += amount
+        print(self.salary)
+
+e1 = Employee("Shashank", 20000)
+e1.display()
+e1.increase_salary(10000)
