981dab05cae19a3b204b241551f048c52edc0fc2
Prashant <prashantdwivedipd7@gmail.com>
2026-05-10 22:32:49 +0530

    created pynative folder to practice questons

--- Pynative/OOP/Rectangle Class with Area & Perimeter.py
+++ Pynative/OOP/Rectangle Class with Area & Perimeter.py
@@ -6,3 +6,17 @@
 
 # Expected Output: Area = 40 and Perimeter = 28
 
+class Rectangle:
+    def __init__(self, length, width):
+        self.lenght = length
+        self.width = width
+
+    def area(self):
+        print("Area of Rectangle is:",self.lenght*self.width)
+
+    def perimeter(self):
+        print("Perimeter of Rectangle is:",2*self.lenght+self.width)
+
+r1 = Rectangle(4,8)
+r1.area()
+r1.perimeter()
