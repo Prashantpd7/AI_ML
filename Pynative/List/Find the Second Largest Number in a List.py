bbde5bb6bbd6ae145feb304f30b7860032dad761
Prashant <prashantdwivedipd7@gmail.com>
2026-05-20 12:15:36 +0530

    new one

--- Pynative/List/Find the Second Largest Number in a List.py
+++ Pynative/List/Find the Second Largest Number in a List.py
@@ -2,6 +2,13 @@
 
 # Exercise Purpose: This exercise teaches you how to process data sets where “rank” matters. It also highlights the importance of handling duplicates. Simply sorting a list does not work if the largest number appears multiple times. It introduces the concept of using Sets to make data unique.
 
-# Given Input: List: [12, 35, 1, 10, 34, 1, 35]
+# Given Input: List: [12, 35, 1, 10, 34, 1, 35,36,36,36,36,37,37]
 
 # Expected Output: Second Largest: 34
+
+lst = [12, 35, 1, 10, 34, 1, 35,38,45,45]
+
+unique = list(set(lst))
+unique.sort()
+print(unique)
+print(unique[-2])
